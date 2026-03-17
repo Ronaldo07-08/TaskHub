5962d3e89b612bde04d20fbb8d521b13b3cb2f38
Ronaldo07-08 <divanov020806@gmail.com>
2026-03-17 17:58:52 +0500

    Дз 3 Атрибуты

---
+++ TaskHub/Api/Hw/Attributes_task/StudentInfoHeadersAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Hw.Attributes_task
+{
+    public class StudentInfoHeadersAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Append("X-Student-Name", "Ivanov Dmitri Igorevich");
+            context.HttpContext.Response.Headers.Append("X-Student-Group", "Ri-240946");
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
